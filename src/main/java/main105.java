7d7cc73ca930a5089fcf81e6648b382d2139887c
zhang <zhang@10.11.246.222>
2018-03-22 19:46:02 -0400

    111

---
+++ src/main/java/main105.java
@@ -0,0 +1,47 @@
+
+
+import java.util.List;
+import java.util.Map;
+import org.patriques.AlphaVantageConnector;
+import org.patriques.TimeSeries;
+import org.patriques.input.timeseries.Interval;
+import org.patriques.input.timeseries.OutputSize;
+import org.patriques.output.AlphaVantageException;
+import org.patriques.output.timeseries.IntraDay;
+import org.patriques.output.timeseries.data.StockData;
+
+
+public class main105 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String apiKey = "71F0P2GUBAVS3SQE";
+    int timeout = 3000;
+    AlphaVantageConnector apiConnector = new AlphaVantageConnector(apiKey, timeout);
+    
+    TimeSeries stockTimeSeries = new TimeSeries(apiConnector);
+    
+    try {
+      IntraDay response = stockTimeSeries.intraDay("MSFT", Interval.ONE_MIN, OutputSize.COMPACT);
+      Map<String, String> metaData = response.getMetaData();
+      System.out.println("Information: " + metaData.get("1. Information"));
+      System.out.println("Stock: " + metaData.get("2. Symbol"));
+      
+      List<StockData> stockData = response.getStockData();
+      stockData.forEach(stock -> {
+        System.out.println("date:   " + stock.getDateTime());
+        System.out.println("open:   " + stock.getOpen());
+        System.out.println("high:   " + stock.getHigh());
+        System.out.println("low:    " + stock.getLow());
+        System.out.println("close:  " + stock.getClose());
+        System.out.println("volume: " + stock.getVolume());
+      });
+    } catch (AlphaVantageException e) {
+      System.out.println("something went wrong");
+    }
+    
+  }
+}
+
