36cd26ea14b9907bef85f77b3d4502a126687104
Patrick Gallager <Patrick Gallager@10.11.7.162>
2018-03-22 20:43:11 -0400

    1st and 2nd Frame

--- src/stockTicker/Stocks.java
+++ src/stockTicker/Stocks.java
@@ -2,15 +2,16 @@ package stockTicker;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Stocks extends JFrame{
 	private static Stocks frame = new Stocks();
-	//private static stockName stockName = new stockName();
+	private static stockName stockName = new stockName();
 	//private static List frame = new List();
 	//private static Description frame = new Description();
 	
-	private JPanel jbList = new JPanel();
-	private JPanel jbName = new JPanel();
+	private JPanel jpList = new JPanel();
+	private JPanel jpName = new JPanel();
 	private JTextField jtfSearch = new JTextField(10);
 	
 	public static void main(String[] args) {
@@ -23,22 +24,39 @@ public class Stocks extends JFrame{
 	}
 	
 	public Stocks(){
+		
+		int b[] = {3,1,4,5,68,9,2};
+		
 		JPanel jpMenu = new JPanel();
 		jpMenu.setLayout(new GridLayout(2,2));
-		jpMenu.add(jbName);
-		jbName.setBackground(Color.black);
+		jpMenu.add(jpName);
+		jpName.setBackground(Color.black);
 		jpMenu.add(jtfSearch);
 		jtfSearch.setSize(150,200);
-		jpMenu.add(jbList);
-		jbList.setBackground(Color.RED);
+		jpMenu.add(jpList);
+		//JPanel jbList = new JPanel();
+		if (b.length > 0){
+			JLabel jlMessage = new JLabel("Stocks");
+			jpList.setLayout(new GridLayout(b.length,1));
+			jpList.add(jlMessage);
+			for(int i = 0; i <b.length; i++){
+				jpList.add(new JButton(""+b[i]));
+			}
+		}
+		else{
+			JLabel jlMessage = new JLabel("Your stocks are here when they are added");
+			jpList.add(jlMessage);
+		}
 		add(jpMenu, BorderLayout.CENTER);
 		
-		/*jtfSearch.addActionListener(new ActionListener(){
+		jtfSearch.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
 				stockName.setTitle("Hello");
+				stockName.setVisible(true);
+				stockName.setSize(300,500);
 			}
-		});*/
+		});
 	}
 
 }
--- src/stockTicker/stockName.java
+++ src/stockTicker/stockName.java
@@ -1,4 +1,4 @@
-package softwareengineerclass;
+package stockTicker;
 
 /**
  *
