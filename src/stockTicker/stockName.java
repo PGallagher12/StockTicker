aa6113cf984a93e3eb23c33ac2039d71779bad22
Chen Zhu <czhu2@washcoll.edu>
2018-03-28 21:57:33 -0400

    Update stockName.java

--- src/stockTicker/stockName.java
+++ src/stockTicker/stockName.java
@@ -1,4 +1,4 @@
-package stockTicker;
+package softwareengineerclass;
 
 /**
  *
@@ -10,24 +10,26 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 public class stockName extends JFrame{
-    private JButton[] myButtons = new JButton[5];
+    private String[] data = {"Item 1", "Item 2", "Item 3",
+        "Item 4", "Item 5", "Item 6"};
+    private int size = data.length;
+    private JButton[] myButtons = new JButton[size];
     public stockName() {
     //Create a panel
-    JPanel p1 = new JPanel(new FlowLayout(FlowLayout.LEFT, 2, 2));
-    //JLabel label = new JLabel("This is a Array");
-    //label.setFont(new Font("Serif", Font.PLAIN, 24));
-    //add(label, BorderLayout.CENTER);
-    String[] data = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"};
-    //p1.add(new JList(data));
-    add(p1, BorderLayout.CENTER);
+        JPanel p1 = new JPanel();
+        p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
+
+        add(p1, BorderLayout.CENTER);
+
+
+        for (int i=0; i<size; i++)
+        {
+            myButtons[i] = new JButton(data[i]);
+            //myButtons[i].setSize(50, 100);
+            p1.add(myButtons[i]);
+        }
+        p1.setBackground(Color.BLACK);
     
-    
-    for (int i=0; i<5; i++)
-    {
-        myButtons[i] = new JButton(Integer.toString(i));
-        myButtons[i].setSize(50, 50);
-        p1.add(myButtons[i]);
-    }
     }
     
 
@@ -37,8 +39,8 @@ public class stockName extends JFrame{
     public static void main(String[] args) {
     // Create a frame and set its properties
     JFrame frame = new stockName();
-    frame.setTitle("BOX");
-    frame.setSize(300, 150);
+    frame.setTitle("Favorite Stocks");
+    frame.setSize(150, 200);
     frame.setLocationRelativeTo(null); // Center the frame
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
